6e4cef5f8802536cddaf062efc6b3b76b4c219e8
chimaev7 <m.kirilov2020@pmg-pernik.com>
2026-01-20 11:50:48 +0200

    inicial commit

---
+++ FightGearShopApp/FightGearShopApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FightGearShopApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
