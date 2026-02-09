654251b17acdc9b59fb4e529dfd0ca93563c752c
chimaev7 <m.kirilov2020@pmg-pernik.com>
2026-02-09 12:38:41 +0200

    Step3(-)

---
+++ FightGearShopApp/FightGearShopApp.Infrastucture/Data/Infrastucture/ApplicationBuilderExtention.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightGearShopApp.Infrastucture.Data.Infrastucture
+{
+    public static class ApplicationBuilderExtention
+    {
+        public static async Task<IApplicationBuilder> PrepareDatabase(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+
+            var services = serviceScope.ServiceProvider;
+
+            await RoleSeeder(services);
+            //  await SeedAdministrator(services);
+
+            return app;
+        }
+
+        private static async Task RoleSeeder(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] roleNames = { "Administrator", "Client" };
+
+            IdentityResult roleResult;
+
+            foreach (var role in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+
+                if (!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+
+}
