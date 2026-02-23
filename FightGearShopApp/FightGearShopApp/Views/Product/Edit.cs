0a7373f6f54c23de48c78765c6fd714d59423fc0
chimaev7 <m.kirilov2020@pmg-pernik.com>
2026-02-23 14:07:54 +0200

    4.16NotDone/5.3,5Done

---
+++ FightGearShopApp/FightGearShopApp/Views/Product/Edit.cs
@@ -0,0 +1,45 @@
+﻿using FightGearShopApp.Infrastucture.Data.Domain;
+using FightGearShopApp.Models.Brand;
+using FightGearShopApp.Models.Category;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace FightGearShopApp.Views.Product
+{
+    public class ProductEditVM
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Required]
+        [Display(Name = "Brand")]
+        public int BrandId { get; set; }
+        public virtual List<BrandPairVM> Brands { get; set; }
+            = new List<BrandPairVM>();
+
+        [Display(Name = "Category")]
+        public int Categoryid { get; set; }
+        public virtual List<CategoryPairVM> Categories { get; set; }
+            = new List<CategoryPairVM>();
+
+        [Display(Name = "Picture")]
+        public string Picture { get; set; } = null!;
+
+        [Range(0, 5000)]
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set;}
+
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Discount")]
+        public decimal Discount { get; set; }
+    }
+
+  
+}
