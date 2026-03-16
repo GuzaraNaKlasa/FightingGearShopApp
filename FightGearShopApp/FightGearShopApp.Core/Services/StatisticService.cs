4dcb02bdbcbd5160376243b1d94f778ca5bf02da
chimaev7 <m.kirilov2020@pmg-pernik.com>
2026-03-16 11:39:51 +0200

    Done(-AF)

---
+++ FightGearShopApp/FightGearShopApp.Core/Services/StatisticService.cs
@@ -0,0 +1,42 @@
+﻿using FightGearShopApp.Core.Contracts;
+using FightGearShopApp.Infrastucture.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightGearShopApp.Core.Services
+{
+    public class StatisticService : IStatisticService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StatisticService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public int CountClients()
+        {
+            return _context.Users.Count()-1;
+        }
+
+        public int CountOrders()
+        {
+            return _context.Orders.Count();
+        }
+
+        public int CountProducts()
+        {
+            return _context.Products.Count();
+        }
+
+        public decimal SumOrders()
+        {
+            var Sum = _context.Orders.Sum(x => x.TotalPrice);
+            return Sum;
+        }
+    }
+}
---
+++ FightGearShopApp/FightGearShopApp/Controllers/StatisticController.cs
@@ -0,0 +1,29 @@
+﻿using FightGearShopApp.Core.Contracts;
+using FightGearShopApp.Models.Statistic;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FightGearShopApp.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private readonly IStatisticService statisticsService;
+       
+        public StatisticController(IStatisticService statisticService)
+        {
+            this.statisticsService = statisticService;
+        }
+
+        public IActionResult Index()
+        {
+            StatisticVM statistics =new StatisticVM();
+
+            statistics.CountClients = statisticsService.CountClients();
+            statistics.CountProducts = statisticsService.CountProducts();
+            statistics.CountOrders = statisticsService.CountOrders();
+            statistics.SumOrders = statisticsService.SumOrders();
+
+            return View(statistics);
+        }
+    }
+}
